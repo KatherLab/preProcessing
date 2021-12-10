cb0f31614ade3de122ac227851bfa23f6403e7bf
Narmin <64002842+narminGhaffari@users.noreply.github.com>
2021-12-10 08:52:34 +0100

    Update extractTiles-ws.py

--- extractTiles-ws.py
+++ extractTiles-ws.py
@@ -10,9 +10,6 @@
 
 from multiprocessing.dummy import Pool as ThreadPool
 from os.path import join, isfile, exists
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import os
 import progressbar
